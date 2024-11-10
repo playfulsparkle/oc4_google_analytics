00c46e8c0d574db819e98ee3d6f1c007bb21abf0
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-10 15:59:26 +0100

    French language

---
+++ src/admin/language/fr-fr/analytics/ps_google_analytics.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Playful Sparkle - Google Analytics (GA4)';
+$_['heading_getting_started']       = 'Démarrage';
+$_['heading_setup']                 = 'Configuration de Google Analytics (GA4)';
+$_['heading_troubleshot']           = 'Dépannage courant';
+$_['heading_faq']                   = 'FAQ';
+$_['heading_contact']               = 'Contacter le support';
+
+// Text
+$_['text_extension']                = 'Extensions';
+$_['text_edit']                     = 'Modifier Google Analytics (GA4)';
+$_['text_success']                  = 'Succès : Vous avez modifié Google Analytics (GA4) !';
+$_['text_getting_started']          = '<p><strong>Aperçu :</strong> Le Playful Sparkle - Google Analytics (GA4) pour OpenCart 4.x vous permet d\'intégrer facilement le suivi Google Analytics (GA4) dans votre boutique OpenCart sans compétences en programmation.</p><p><strong>Exigences :</strong> Assurez-vous de disposer d\'un compte Google Analytics, d\'un ID de balise Google valide, et d\'utiliser une version compatible de OpenCart 4.x+, PHP 7.4+.</p>';
+$_['text_setup']                    = '<p><strong>Liaison de Google Analytics :</strong> Connectez-vous à votre <a href="https://analytics.google.com" target="_blank" rel="noopener noreferrer">compte Google Analytics</a>, accédez à la section Administration, sélectionnez votre propriété, et récupérez votre ID de balise Google.</p>';
+$_['text_troubleshot']              = '<ul><li><strong>ID de balise Google non fonctionnel :</strong> Vérifiez que vous avez saisi le bon ID de balise Google dans les paramètres de l\'extension.</li><li><strong>Retard des données dans GA4 :</strong> Notez que les données peuvent mettre quelques heures à apparaître dans Google Analytics.</li><li><strong>Problèmes de compatibilité :</strong> Assurez-vous que votre version d\'OpenCart est à jour et compatible avec l\'extension.</li></ul>';
+$_['text_faq']                      = '<details><summary>Que faire si je ne vois pas mes données dans Google Analytics ?</summary>Vérifiez votre ID de balise Google et attendez quelques heures pour que les données apparaissent dans GA4.</details><details><summary>Comment vérifier si l\'extension fonctionne ?</summary>Utilisez l\'extension Google Tag Assistant pour Chrome afin de vérifier votre configuration.</details><details><summary>Cette extension est-elle compatible avec d\'autres extensions ?</summary>Cette extension est généralement compatible, mais il est préférable de la tester avec les autres extensions installées sur votre site.</details>';
+$_['text_contact']                  = '<p>Pour toute assistance supplémentaire, veuillez contacter notre équipe de support :</p><ul><li><strong>Contact :</strong> <a href="mailto:%s">%s</a></li><li><strong>Documentation :</strong> <a href="%s" target="_blank" rel="noopener noreferrer">Documentation utilisateur</a></li></ul>';
+
+// Tab
+$_['tab_general']                   = 'Général';
+$_['tab_help_and_support']          = 'Aide &amp; Support';
+
+// Entry
+$_['entry_status']                  = 'Statut';
+$_['entry_google_tag_id']           = 'ID de balise Google';
+
+// Help
+$_['help_google_tag_id_locate']     = 'Pour trouver votre ID de balise Google, connectez-vous à votre <a href="https://analytics.google.com" target="_blank" rel="external noopener noreferrer">compte Analytics</a>. Allez dans la section Administration, sélectionnez la propriété que vous souhaitez suivre, et trouvez votre ID de balise Google. Il commencera par "G-" suivi d\'une combinaison unique de lettres et de chiffres, comme "G-XXXXXXXXXX." <a href="https://support.google.com/analytics/answer/9539598?hl=fr" target="_blank" rel="external noopener noreferrer">Instructions détaillées ici</a>.';
+
+// Error
+$_['error_permission']              = 'Avertissement : Vous n\'avez pas l\'autorisation de modifier les paramètres de Google Analytics !';
+$_['error_google_tag_id']           = 'Le champ ID de balise Google est requis. Veuillez entrer votre ID Google Analytics.';
+$_['error_google_tag_id_invalid']   = 'Le format de l\'ID de balise Google est incorrect. Assurez-vous qu\'il suit le format G-XXXXXXXXXX.';
