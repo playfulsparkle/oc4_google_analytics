7759c3213f5c9d5a6a564dde10c999addfc93747
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-10 15:57:38 +0100

    Spanish language

---
+++ src/admin/language/es-es/analytics/ps_google_analytics.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Playful Sparkle - Google Analytics (GA4)';
+$_['heading_getting_started']       = 'Primeros Pasos';
+$_['heading_setup']                 = 'Configuración de Google Analytics (GA4)';
+$_['heading_troubleshot']           = 'Solución de Problemas Comunes';
+$_['heading_faq']                   = 'Preguntas Frecuentes';
+$_['heading_contact']               = 'Contactar Soporte';
+
+// Text
+$_['text_extension']                = 'Extensiones';
+$_['text_edit']                     = 'Editar Google Analytics (GA4)';
+$_['text_success']                  = 'Éxito: ¡Has modificado Google Analytics (GA4)!';
+$_['text_getting_started']          = '<p><strong>Resumen:</strong> El Playful Sparkle - Google Analytics (GA4) para OpenCart 4.x te permite integrar fácilmente el seguimiento de Google Analytics (GA4) en tu tienda OpenCart sin necesidad de habilidades de programación.</p><p><strong>Requisitos:</strong> Asegúrate de tener una cuenta de Google Analytics, un ID de Google Tag válido y que estés utilizando una versión compatible de OpenCart 4.x+, PHP 7.4+.</p>';
+$_['text_setup']                    = '<p><strong>Vinculando Google Analytics:</strong> Inicia sesión en tu <a href="https://analytics.google.com" target="_blank" rel="noopener noreferrer">cuenta de Google Analytics</a>, navega a la sección de Administración, selecciona tu propiedad y recupera tu ID de Google Tag.</p>';
+$_['text_troubleshot']              = '<ul><li><strong>ID de Google Tag no funciona:</strong> Verifica que hayas ingresado el ID de Google Tag correcto en la configuración de la extensión.</li><li><strong>Retraso de datos en GA4:</strong> Ten en cuenta que los datos pueden tardar algunas horas en aparecer en Google Analytics.</li><li><strong>Problemas de compatibilidad:</strong> Asegúrate de que tu versión de OpenCart esté actualizada y sea compatible con la extensión.</li></ul>';
+$_['text_faq']                      = '<details><summary>¿Qué hago si no veo mis datos en Google Analytics?</summary>Verifica tu ID de Google Tag y espera algunas horas para que los datos se carguen en GA4.</details><details><summary>¿Cómo puedo verificar si la extensión está funcionando?</summary>Utiliza la extensión de Google Tag Assistant para Chrome para verificar tu configuración.</details><details><summary>¿Es compatible esta extensión con otras extensiones?</summary>Generalmente, esta extensión es compatible, pero es recomendable probarla con otras extensiones instaladas en tu sitio.</details>';
+$_['text_contact']                  = '<p>Para más asistencia, por favor contacta a nuestro equipo de soporte:</p><ul><li><strong>Contacto:</strong> <a href="mailto:%s">%s</a></li><li><strong>Documentación:</strong> <a href="%s" target="_blank" rel="noopener noreferrer">Documentación del Usuario</a></li></ul>';
+
+// Tab
+$_['tab_general']                   = 'General';
+$_['tab_help_and_support']          = 'Ayuda y Soporte';
+
+// Entry
+$_['entry_status']                  = 'Estado';
+$_['entry_google_tag_id']           = 'ID de Google Tag';
+
+// Help
+$_['help_google_tag_id_locate']     = 'Para encontrar tu ID de Google Tag, inicia sesión en tu <a href="https://analytics.google.com" target="_blank" rel="external noopener noreferrer">cuenta de Analytics</a>. Ve a la sección de Administración, selecciona la propiedad que deseas rastrear y encuentra tu ID de Google Tag. Comenzará con "G-" seguido de una combinación única de letras y números, como "G-XXXXXXXXXX". <a href="https://support.google.com/analytics/answer/9539598?hl=es" target="_blank" rel="external noopener noreferrer">Instrucciones más detalladas aquí</a>.';
+
+// Error
+$_['error_permission']              = 'Advertencia: ¡No tienes permiso para modificar la configuración de Google Analytics!';
+$_['error_google_tag_id']           = 'El campo ID de Google Tag es obligatorio. Por favor, ingresa tu ID de Google Analytics.';
+$_['error_google_tag_id_invalid']   = 'El formato del ID de Google Tag es incorrecto. Asegúrate de que siga el formato G-XXXXXXXXXX.';
